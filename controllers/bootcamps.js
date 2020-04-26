a58c0d6b9dcb563c3add74b2d012b49fc227ef5a
koftov <ahronlu@gmail.com>
2020-04-26 14:16:11 +0300

    course routes controllers

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,5 @@
 const ErrorResponse = require('../utils/errorResponse');
-const asyncHandler = require('../middelware/async');
+const asyncHandler = require('../middleware/async');
 const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
 
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -8,7 +8,13 @@ const {
   getBootcampInRadius,
 } = require('../controllers/bootcamps');
 
+// Include other resource routers
+const courseRouter = require('./courses');
+
 const router = express.Router();
+
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/').get(getBootcamps).post(createBootcamp);
 router
